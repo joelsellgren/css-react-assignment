154a3702ead0004799e28c452d070ab373f9dc3b
Joel Sellgren <joel.sellgren@chasacademy.se>
2023-05-03 15:20:20 +0200

    changed pictures + functions to slider component

--- src/components/main/Slider.jsx
+++ src/components/main/Slider.jsx
@@ -2,22 +2,46 @@ import { useState } from "react"
 
 const Slider = () => {
 
-  const slides = [
-    {public: '/images/Shoes2.jpg'},
-    {public: '/images/Shoes3.jpg'},
-    {public: '/images/Shoes4.jpg'},
-    {public: '/images/Shoes5.jpg'},
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0)
+  const [activeLargeSlideIndex, setActiveLargeSlideIndex] = useState(0)
+
+  const handleSlideClick = (index) => {
+    setActiveSlideIndex(index)
+    setActiveLargeSlideIndex(index)
+  }
+
+  const smallSlides = [
+    {src: '/images/Shoes1.jpg'},
+    {src: '/images/Shoes2.jpg'},
+    {src: '/images/Shoes3.jpg'},
+    {src: '/images/Shoes4.jpg'}, 
+  ]
+
+  const largeSlides = [
+    {src: '/images/Shoes1large.jpg'},
+    {src: '/images/Shoes2large.jpg'},
+    {src: '/images/Shoes3large.jpg'},
+    {src: '/images/Shoes4large.jpg'}, 
   ]
 
   return (
     <div>
         <div className="flex flex-col items-center mt-24">
-            <img src="/images/Shoes1.jpg" alt="Picture of shoes" />
+            <img
+              src={largeSlides[activeLargeSlideIndex].src}
+              className="rounded-[15px]"
+              alt="Picture of shoes" />
             <div className="flex">
-                <img className="m-4 rounded-[15px] hover:cursor-pointer hover:opacity-50" src="/images/Shoes5.jpg" alt="Picture of shoes" onClick={() => console.log("clicked")}/>
-                <img className="m-4 rounded-[15px] hover:cursor-pointer hover:opacity-50" src="/images/Shoes2.jpg" alt="Picture of shoes" onClick={() => console.log("clicked")}/>
-                <img className="m-4 rounded-[15px] hover:cursor-pointer hover:opacity-50" src="/images/Shoes3.jpg" alt="Picture of shoes" onClick={() => console.log("clicked")}/>
-                <img className="m-4 rounded-[15px] hover:cursor-pointer hover:opacity-50" src="/images/Shoes4.jpg" alt="Picture of shoes" onClick={() => console.log("clicked")}/>
+                {smallSlides.map((slide, index) => (
+                  <img
+                    key={index}
+                    className={`m-4 rounded-[15px] hover:cursor-pointer hover:opacity-50 ${
+                      activeSlideIndex === index && "opacity-50 border-2 border-orange"}`}
+                    src={slide.src}
+                    alt="Picture of shoes"
+                    onClick={() => handleSlideClick(index)}
+                  />
+                ))}
             </div>
         </div>
     </div>

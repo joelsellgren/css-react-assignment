ca9f7a6e7f6da62ee4f8ed6b48a2558cf3de24e6
Joel Sellgren <joel.sellgren@hotmail.com>
2023-05-07 22:22:25 +0200

    same as last commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,11 @@ import Logo from "./Logo"
 import Navbar from "./Navbar"
 import Cart from "./Cart"
 import Login from "./Login"
-import PropTypes from "prop-types"
 
-const Header = (props) => {
 
-  const {updateCart, setUpdateCart} = props;
+const Header = () => {
+
+
 
   return (
     <div className="flex ml-40 mr-40 h-28 justify-between border-b-2 border-darkGrey/50">
@@ -15,17 +15,12 @@ const Header = (props) => {
             <Navbar />
         </div>
         <div className="flex items-center">
-            <Cart updateCart={updateCart} setUpdateCart={setUpdateCart} />
+            <Cart />
             <Login />
         </div>
     </div>
   )
 }
-
-Header.propTypes = {
-  updateCart: PropTypes.func,
-  setUpdateCart: PropTypes.array
-};
 
 
 export default Header
